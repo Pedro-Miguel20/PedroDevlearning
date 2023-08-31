a1ff6734e05d5f17cc26674efb471703e37e886c
Pedro_Miguel <106206370+Pedro-Miguel20@users.noreply.github.com>
2023-08-31 18:25:18 -0400

    Update PalindromoChecker.js
    Agora funciona

--- PalindromoChecker.js
+++ PalindromoChecker.js
@@ -1,15 +1,18 @@
 function palindrome(str) {
   const without = str.replace(/[^a-zA-Z0-9]/g, '');
   console.log(without);
-  const digits = without.toString().split('');
+  let lower  = without.toLowerCase();
+  const digits = lower.toString().split('');
   digits.reverse();
   const reversed = digits.join(''); 
   console.log(reversed)
-  if (reversed === str){
-    console.log("sim")
+  if (reversed === lower){
+    console.log("é um palindromo")
     return true;
   }else{
-    console.log("não")
+    console.log("não é")
     return false;
   }
 }
+
+palindrome("_eye");
