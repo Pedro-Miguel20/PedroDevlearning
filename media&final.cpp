cf379edea9e95a8238399e935ebcab8ff41a6f2d
Pedro_Miguel <106206370+Pedro-Miguel20@users.noreply.github.com>
2023-09-12 08:51:01 -0400

    media&final.cpp

--- media&final.cpp
+++ media&final.cpp
@@ -4,6 +4,9 @@
 
     int main()
 {
+    double repetir = 1;
+    while(repetir == 1){
+    repetir = 0;
     float math1;
     float math2;
     float math3;
@@ -20,15 +23,17 @@
     float sum = math1 + math2 + math3 + math4;
     float media = sum/4;
     if ( media > 7){
-			printf("Aprovado media = %f", media);
+			printf("Aprovado media = %.2f", media);
 	} else {
-			printf("De exame, media final = %f", media);
+			printf("De exame, media final = %.2f", media);
 			printf("\n Digite a nota do exame: ");
 	        scanf("%f",&exam);
-            float final = (exam + media/2);
+            float final = (exam + media)/2;
 		    if (final > 5){
-		        printf("Aprovado media final = %f", final);
+		        printf("Aprovado media final = %.2f", final);
 	        } else {
-			    printf("reprovado, media final = %f", final);}
-	}
+			    printf("reprovado, media final = %.2f", final);}}
+	
+	printf("\n Se quiser repetir clique 1: ");
+    scanf("%le",&repetir);}
 }
